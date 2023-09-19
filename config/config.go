d4be5d894622d9f67d51913faa1c7c218a605944
Wira Satria Negara <wirasatrian@gmail.com>
2023-09-19 16:03:16 +0800

    synchronize variable with railway project

--- config/config.go
+++ config/config.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 
 	"github.com/wirasatrian/go-restapi-gin/models"
+	"github.com/wirasatrian/go-restapi-gin/utils"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDataBase() *gorm.DB {
-	username := "root"
-	password := ""
-	host := "tcp(127.0.0.1:3306)"
-	database := "database_movie"
+	host := utils.Getenv("DATABASE_HOST", "127.0.0.1")
+	port := utils.Getenv("DATABASE_PORT", "3306")
+	database := utils.Getenv("DATABASE_NAME", "database_movie")
+	username := utils.Getenv("DATABASE_USERNAME", "root")
+	password := utils.Getenv("DATABASE_PASSWORD", "")
 
-	dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
